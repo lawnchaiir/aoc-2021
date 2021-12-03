b648580575666d0d49e87f7158a074587f82b339
lawnchaiir <lawnchaiir@users.noreply.github.com>
2021-12-03 12:02:55 -0500

    day 1 part 2

--- 12-1/day1.py
+++ 12-1/day1.py
@@ -11,3 +11,15 @@ for i in range(1, len(lines)):
         count += 1
 
 print(count)
+
+prev_sum = 0
+count = 0
+for i in range(3, len(lines)):
+    sl = lines[i - 3: i]
+    curr_sum = sum(sl)
+    if curr_sum > prev_sum:
+        count += 1
+    prev_sum = curr_sum
+
+print(count)
+
