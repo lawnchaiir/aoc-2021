fbe18dcd79f46600ffbb133e4adba7c6578a5c7e
lawnchaiir <lawnchaiir@users.noreply.github.com>
2021-12-03 14:27:24 -0500

    Day 2 solved!

---
+++ 12-2/Day2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+
+namespace Day2
+{
+    class Program
+    {
+        static int X = 0;
+        static int Depth = 0;
+        static int Aim = 0;
+
+        static void Main(string[] args)
+        {
+            foreach (string line in File.ReadLines(@"..\..\..\..\input.txt"))
+            {
+                ReadOnlySpan<char> spanLine = line.AsSpan();
+                int space = spanLine.IndexOf(" ");
+                string command = spanLine.Slice(0, space).ToString();
+                int amount = int.Parse(spanLine[(space + 1)..]);
+
+                PerformCommandPt2(command, amount);
+            }
+
+            Console.WriteLine((X, Depth));
+            Console.WriteLine(X * Depth);
+        }
+
+        private static void PerformCommandPt1(string command, int amount)
+        {
+            switch (command)
+            {
+                case "forward":
+                {
+                    X += amount;
+                }
+                break;
+                case "down":
+                {
+                    Depth += amount;
+                }
+                break;
+                case "up":
+                {
+                    amount = Math.Min(amount, Depth);
+                    Depth -= amount;
+                }
+                break;
+            }
+        }
+
+        private static void PerformCommandPt2(string command, int amount)
+        {
+            switch (command)
+            {
+                case "forward":
+                {
+                    X += amount;
+                    int depthChange = amount * Aim;
+                    Depth += depthChange;
+                }
+                break;
+                case "down":
+                {
+                    Aim += amount;
+                }
+                break;
+                case "up":
+                {
+                    Aim -= amount;
+                }
+                break;
+            }
+        }
+    }
+}
