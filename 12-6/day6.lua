124409f753d5d1e7edb2096ddf501c006df05f45
lawnchaiir <lawnchaiir@users.noreply.github.com>
2021-12-06 22:28:57 -0500

    day 6 solution part 2. Since the problem space got MUCH larger, I had to shift my approach quite a bit. Also, if you had told me that the best solution for today would be a 0-based array, I would most certainly not have chosen Lua

--- 12-6/day6.lua
+++ 12-6/day6.lua
@@ -6,27 +6,36 @@ f:close()
 
 local lanternFish = {}
 for n in data:gmatch("%d+") do
-    table.insert(lanternFish, n)
+    table.insert(lanternFish, tonumber(n))
 end
 
-local days = 18
+local fishCounts = {}
+-- use more sensible table indexing
+for i = 0, 8 do
+    fishCounts[i] = 0
+end
 
-local fishCount = 0
+for k, v in ipairs(lanternFish) do
+    fishCounts[v] = fishCounts[v] + 1
+end
 
-
---print(table.concat(lanternFish, ","))
-for day = 0, days - 1 do
-    for k, fish in pairs(lanternFish) do
-        if fish == 0 then
-            fish = 6
-            lanternFish[k] = fish
-            table.insert(lanternFish, 9) -- The rules say add an 8 and then skip a day ... or we could add 9
-        else
-            fish = fish - 1
-            lanternFish[k] = fish
-        end
+local days = 256
+for day = 1, days do
+    local readyFish = fishCounts[0]
+    for i = 0, 8 do
+        fishCounts[i] = fishCounts[i + 1]
     end
-    --print(table.concat(lanternFish, ","))
+
+    -- each fish that was at day 0 will have another fish, so start them up at day 8
+    fishCounts[8] = readyFish
+
+    -- all of our day 0 fish now move back to day 6
+    fishCounts[6] = readyFish + fishCounts[6]
 end
 
-print(#lanternFish)
+local sum = 0
+for k,v in pairs(fishCounts) do
+    sum = sum + v
+end
+
+print(sum)
